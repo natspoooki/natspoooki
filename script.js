25620c75e56d793d38eb717f246006fbef11210d
tomás c <112723345+natspoooki@users.noreply.github.com>
2025-05-15 14:33:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const bpmText = document.getElementById("bpmText");
 const heart = document.getElementById("heart");
 
 function updateBPM(bpm) {
+  // Since each "badum" is one animation cycle,
+  // calculate interval to match bpm (1 beat = 1 badum)
   const duration = (60 / bpm).toFixed(2);
   heart.style.animationDuration = `${duration}s`;
   bpmText.textContent = `BPM: ${bpm}`;
